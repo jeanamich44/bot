e514fde04972726cd40240968829082cfcd0e77f
TonNom <tonemail@email.com>
2026-07-31 01:06:32 +0200

    Mise à jour avec la clé marchand OxaPay validée (UWUEMJ-HAHWDD-IYNN8Z-GDQ94H)

--- config.cs
+++ config.cs
@@ -5,7 +5,7 @@ namespace UgcBotTG
     internal class config
     {
         public readonly static string apiUrl = "https://api.oxapay.com/v1/payment/invoice";
-        public readonly static string apiKey = Environment.GetEnvironmentVariable("OXAPAY_API_KEY") ?? "XZSCYQ-MDQCKO-1VNVKO-BBCWR7";
+        public readonly static string apiKey = Environment.GetEnvironmentVariable("OXAPAY_API_KEY") ?? "UWUEMJ-HAHWDD-IYNN8Z-GDQ94H";
 
         public static Dictionary<string, string> PayementLink = new Dictionary<string, string>();
         public static List<string> IdPaiement = new List<string>();
