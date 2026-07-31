7441a1476f3876896dd273249ead5d579ce0d275
TonNom <tonemail@email.com>
2026-07-31 16:48:44 +0200

    Ajout des contacts admin @RheyyFondaa et @NtRheyyTech dans la vue Profil Utilisateur

--- Services/profile.cs
+++ Services/profile.cs
@@ -2,6 +2,7 @@ using System;
 using Telegram.Bot.Types;
 using Telegram.Bot;
 using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.Enums;
 
 namespace ChezRheyyBot
 {
@@ -11,22 +12,36 @@ namespace ChezRheyyBot
         {
             try
             {
-                await botClient.DeleteMessageAsync(config.CurrentChatId, int.Parse(config.IdMessage[config.CurrentChatId]) - 1);
+                int result = config.UserSave.FindIndex(tuple => tuple.Item1 == long.Parse(config.CurrentChatId));
+                double solde = result != -1 ? config.UserSave[result].Item3 : 0.0;
+                int achats = result != -1 ? config.UserSave[result].Item2 : 0;
 
                 var inlineKeyboard = new InlineKeyboardMarkup(new[]
                 {
                     new[]
                     {
-                        InlineKeyboardButton.WithCallbackData("🛒 Carrefour", "iCarrefourProfile")
-                    },
+                        InlineKeyboardButton.WithCallbackData("🏠 Accueil", "iHome")
+                    }
                 });
 
-                await botClient.SendTextMessageAsync(config.CurrentChatId, $"Bonjour @{config.CurrentPseudo}\n\n", replyMarkup: inlineKeyboard);
+                string message = $"👤 *PROFIL UTILISATEUR*\n\n" +
+                                 $"🆔 ID : `{config.CurrentChatId}`\n" +
+                                 $"👤 Pseudo : @{config.CurrentPseudo}\n" +
+                                 $"💰 Solde : {solde}€\n" +
+                                 $"🛒 Achats effectués : {achats}\n\n" +
+                                 $"💬 *Besoin Aide ? Contactez un Admin :*\n" +
+                                 $"@RheyyFondaa\n" +
+                                 $"@NtRheyyTech";
+
+                try
+                {
+                    await botClient.DeleteMessageAsync(config.CurrentChatId, int.Parse(config.IdMessage[config.CurrentChatId]) - 1, cancellationToken: cancellationToken);
+                }
+                catch { }
+
+                await botClient.SendTextMessageAsync(config.CurrentChatId, message, parseMode: ParseMode.Markdown, replyMarkup: inlineKeyboard, cancellationToken: cancellationToken);
             }
-            catch
-            {
-                await botClient.DeleteMessageAsync(config.CurrentChatId, int.Parse(config.IdMessage[config.CurrentChatId]));
-            }
+            catch { }
         }
     }
 }
