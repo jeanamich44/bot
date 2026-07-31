f246eb9333497d8d3509fa3e3582d334f6058356
TonNom <tonemail@email.com>
2026-07-31 17:22:09 +0200

    Suppression du reliquat legacy generator.exe et optimisation de l'envoi de code-barres Carrefour

--- Services/achat.cs
+++ Services/achat.cs
@@ -1,5 +1,9 @@
-using System.Data.Common;
-using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -10,22 +14,21 @@ namespace ChezRheyyBot
 {
     internal class achat
     {
-        public static async Task DemandeAchat(string message, ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        public static async Task DemandeAchat(string messageSystem, ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             try
             {
                 var msg = update.CallbackQuery.Data.Split('_');
-
+                var item = DataBase.ObtenirStockParId(int.Parse(msg[1]));
                 int result = config.UserSave.FindIndex(tuple => tuple.Item1 == long.Parse(config.CurrentChatId));
 
                 if (result == -1)
                 {
+                    await botClient.SendTextMessageAsync(config.CurrentChatId, "Utilisateur introuvable.", cancellationToken: cancellationToken);
                     return;
                 }
 
                 var ancienTuple = config.UserSave[result];
-
-                var item = DataBase.ObtenirStockParId(int.Parse(msg[1]));
 
                 if (item != null)
                 {
@@ -34,9 +37,10 @@ namespace ChezRheyyBot
 
                     if (solde - double.Parse(prix) < 0)
                     {
-                        await botClient.SendTextMessageAsync(config.CurrentChatId, $"Votre solde de {solde}€ ne suffit pas");
+                        await botClient.SendTextMessageAsync(config.CurrentChatId, $"Votre solde de {solde}€ ne suffit pas.", cancellationToken: cancellationToken);
                         return;
                     }
+
                     var chatid = config.CurrentChatId;
 
                     if (DataBase.SupprimerStockParId(int.Parse(msg[1])))
@@ -45,154 +49,92 @@ namespace ChezRheyyBot
                         config.UserSave[result] = Tuple.Create(ancienTuple.Item1, ancienTuple.Item2 + 1, nouveauSolde, ancienTuple.Item4);
                         DataBase.SauvegarderUtilisateurIndividuel(ancienTuple.Item1);
 
-
                         Random rnd = new Random();
                         int nombre = rnd.Next(100, 999);
+                        string fileName = $"carr{chatid}_{nombre}.png";
 
-                        if(item.Brand == "accor")
-                        {
-                            await EnregistrerLogsVendu(item.Code, item.Value, prix, config.CurrentChatId, item.Brand);
-                            await AvertirAchat(botClient, update, cancellationToken, prix, item.Brand);
+                        codebarre.GenerateBarcode(item.Code, fileName);
 
-                            await botClient.SendTextMessageAsync(config.CurrentChatId, $"Logs AccorHotel:\n📧 Email:<code>{item.Code}</code>\n🔑 Password:<code>{item.Pin}</code>\nPoints:{item.Value}",parseMode:ParseMode.Html);
-
-                            return;
-                        }
-
-                        if(item.Brand == "carr")
-                        {
-                            codebarre.GenerateBarcode(item.Code, $"carr{chatid}_{nombre}.png");
-                        }
-                        else 
-                        {
-                            System.Diagnostics.Process.Start("generator.exe", $"{item.Code} {chatid}_{nombre}.png {item.Brand}");
-                        }
-
-
-                        await EnregistrerLogsVendu(item.Code,item.Value, prix, config.CurrentChatId,item.Brand);
-                        await AvertirAchat(botClient, update, cancellationToken, prix,item.Brand);
-
-                        if(item.Brand == "carr")
-                        {
-                            await EnvoyerQrCode(chatid, botClient, update, cancellationToken,item.Code,item.Pin);
-                        }
-                        else
-                        {
-
-
-                            await EnvoyerQrCode(chatid, botClient, update, cancellationToken);
-                        }
+                        await EnregistrerLogsVendu(item.Code, item.Pin, item.Value, prix, config.CurrentChatId, item.Brand);
+                        await AvertirAchat(botClient, update, cancellationToken, prix, item.Brand);
+                        await EnvoyerBarcodeDirect(fileName, chatid, botClient, update, cancellationToken, item.Code, item.Pin);
                         return;
                     }
                 }
                 else
                 {
-                    await botClient.SendTextMessageAsync(config.CurrentChatId, "Le produit n'est plus disponible");
+                    await botClient.SendTextMessageAsync(config.CurrentChatId, "Le produit n'est plus disponible.", cancellationToken: cancellationToken);
                     return;
                 }
             }
-            catch
-            {
-                return;
-            }
+            catch { }
         }
 
-        private static async Task<bool> EnvoyerQrCode(string clientId, ITelegramBotClient botClient, Update update, CancellationToken cancellationToken, string carte = "",string pin = "")
+        private static async Task<bool> EnvoyerBarcodeDirect(string fileName, string clientId, ITelegramBotClient botClient, Update update, CancellationToken cancellationToken, string carte = "", string pin = "")
         {
             try
             {
-                while (true)
+                var inlineKeyboard = new InlineKeyboardMarkup(new[]
                 {
-                    var inlineKeyboard = new InlineKeyboardMarkup(new[]
-           {
                     new[]
                     {
-                        InlineKeyboardButton.WithCallbackData("Home", "iHome")
+                        InlineKeyboardButton.WithCallbackData("🏠 Accueil", "iHome")
                     }
                 });
-                    string dossierActuel = Directory.GetCurrentDirectory();
-                    string[] fichiersPng = Directory.GetFiles(dossierActuel, "*.png");
 
-                    foreach (string fichier in fichiersPng)
+                if (System.IO.File.Exists(fileName))
+                {
+                    using (var stream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
                     {
-                        try
-                        {
-                            using (var stream = new FileStream(fichier, FileMode.Open, FileAccess.Read))
-                            {
-                                var photo = new InputOnlineFile(stream, Path.GetFileName(fichier));
+                        var photo = new InputOnlineFile(stream, Path.GetFileName(fileName));
+                        string pinText = string.IsNullOrWhiteSpace(pin) ? "" : $"\n🔑 PIN : <code>{pin}</code>";
+                        string caption = $"✅ <b>Merci pour votre achat !</b>\n\n💳 Carte Carrefour : <code>{carte}</code>{pinText}";
 
-
-                                if (photo.FileName.Contains("_"))
-                                {
-                                    var idphoto = photo.FileName.Split('_');
-
-                                    if (idphoto[0].Contains("carr"))
-                                    {
-                                        idphoto[0] = idphoto[0].Remove(0, 4);
-
-                                        await botClient.SendPhotoAsync(
-                                    chatId: idphoto[0],
-                                    caption: $"Merci de ton achat, Carte:{carte}:{pin}",
-                                    photo: photo,
-                                    parseMode: ParseMode.Html,
-                                    replyMarkup: inlineKeyboard);
-                                    }
-                                    else
-                                    {
-                                        await botClient.SendPhotoAsync(
-                                    chatId: idphoto[0],
-                                    caption: $"Merci de ton achat",
-                                    photo: photo,
-                                    parseMode: ParseMode.Html,
-                                    replyMarkup: inlineKeyboard);
-                                    } 
-                                }
-                            }
-                            System.IO.File.Delete(fichier);
-                            return true;
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine("❌ Erreur en envoyant " + fichier + " : " + ex.Message);
-                        }
+                        await botClient.SendPhotoAsync(
+                            chatId: clientId,
+                            photo: photo,
+                            caption: caption,
+                            parseMode: ParseMode.Html,
+                            replyMarkup: inlineKeyboard,
+                            cancellationToken: cancellationToken);
                     }
+
+                    try
+                    {
+                        System.IO.File.Delete(fileName);
+                    }
+                    catch { }
+
+                    return true;
                 }
             }
-            catch
-            {
-
-            }
+            catch { }
 
             return false;
         }
 
-        public static async Task AvertirAchat(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken,string prix,string brand)
+        public static async Task AvertirAchat(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken, string montant, string brand)
         {
-            try
+            foreach (var id in config.idAdmins)
             {
-                foreach(var id in config.idAdmins)
+                try
                 {
-                    await botClient.SendTextMessageAsync(id, $"[+] Nouvelle commandes: Id:{config.CurrentChatId} | User:{config.CurrentPseudo} | Prix: {prix} | Categorie: {brand}");
+                    await botClient.SendTextMessageAsync(id, $"🛒 *ACHAT EFFECTUÉ*\n\nUser: `{config.CurrentChatId}`\nMarque: *{brand}*\nPrix: {montant}€", parseMode: ParseMode.Markdown, cancellationToken: cancellationToken);
                 }
-            }
-            catch
-            {
-
+                catch { }
             }
         }
-        public static async Task EnregistrerLogsVendu(string carte,string valeur,string prix,string id,string brand)
+
+        public static async Task EnregistrerLogsVendu(string carte, string pin, string solde, string prix, string userId, string brand)
         {
             try
             {
-                long.TryParse(config.CurrentChatId, out long userId);
-                int.TryParse(valeur, out int val);
-                double.TryParse(prix, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double prx);
-                DataBase.EnregistrerTransaction(userId, brand, carte, "", val, prx);
+                int.TryParse(solde, out int val);
+                double.TryParse(prix, out double p);
+                long.TryParse(userId, out long uId);
+                DataBase.EnregistrerTransaction(uId, brand, carte, pin ?? "", val, p);
             }
-            catch
-            {
-
-            }
+            catch { }
         }
     }
 }
